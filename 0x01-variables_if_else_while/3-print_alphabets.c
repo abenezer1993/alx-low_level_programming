49b9f94753ea014d1b76c6217f464c46549dbcfa
abenezer1993 <abenitak@gmail.com>
2023-10-06 15:10:18 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints the character both on Upper and Lower cases
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	for ( i = 'A'; i <= 'Z'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
